be041f3f2b0e1228da336d4f23e705a02f88abe0
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@gmail.com>
2024-10-14 05:29:37 +0200

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -5,6 +5,15 @@ const Geometry = [
     new Polygon(236, 170,[255,0,0],[ new Vertex(-4, -146),new Vertex(-6, -151),new Vertex(-9, -159),new Vertex(-15, -165),new Vertex(-19, -167),new Vertex(-25, -169),new Vertex(-25, -170),new Vertex(3, -170),new Vertex(3, -146), ], 'ramp_tr'),
     new Polygon(185, 6,[255,0,0],[ new Vertex(-9, 9),new Vertex(-9, -6),new Vertex(19, -6),new Vertex(19, -5),new Vertex(13, -3),new Vertex(9, -1),new Vertex(3, 5),new Vertex(1, 9), ], 'ramp_tl'),
     new Polygon(211, 1,[255,0,0],[ new Vertex(0, 0),new Vertex(-7, 0),new Vertex(-7, 0),new Vertex(0, 0), ], 'ramp_tt'),
+    new Polygon(207, 15,[255,0,0],[ new Vertex(-11, -3),new Vertex(-6, -6),new Vertex(2, -7),new Vertex(6, -6),new Vertex(11, -4),new Vertex(15, 1),new Vertex(16, 5),new Vertex(-15, 5), ], 'new poly'),
+    new Polygon(206, 29,[255,0,0],[ new Vertex(-14, -9),new Vertex(17, -9),new Vertex(17, 11),new Vertex(-14, 11), ], 'new poly'),
+    new Polygon(123, 7,[255,0,0],[ new Vertex(-54, 8),new Vertex(-54, -7),new Vertex(53, -7),new Vertex(53, 8), ], 'new poly'),
+    new Polygon(206, 74,[255,0,0],[ new Vertex(17, -34),new Vertex(17, 32),new Vertex(-14, 32),new Vertex(-14, -34), ], 'new poly'),
+    new Polygon(0, 24,[255,0,0],[ new Vertex(0, -1),new Vertex(7, -1),new Vertex(7, 242),new Vertex(0, 242), ], 'new poly'),
+    new Polygon(29, 300,[255,0,0],[ new Vertex(-22, -34),new Vertex(38, -3),new Vertex(41, 0),new Vertex(42, 3),new Vertex(42, 5),new Vertex(35, 19),new Vertex(-29, 19),new Vertex(-29, -34), ], 'new poly'),
+    new Polygon(143, 319,[255,0,0],[ new Vertex(0, 0),new Vertex(-7, -14),new Vertex(-7, -17),new Vertex(-5, -20),new Vertex(-3, -22),new Vertex(58, -52),new Vertex(80, -52),new Vertex(80, 0), ], 'new poly'),
+    new Polygon(201, 267,[255,0,0],[ new Vertex(-1, 0),new Vertex(-1, -153),new Vertex(22, -153),new Vertex(22, 0), ], 'new poly'),
+    new Polygon(208, 110,[255,0,0],[ new Vertex(-8, 4),new Vertex(-8, 4),new Vertex(-11, -3),new Vertex(15, -3),new Vertex(15, 4), ], 'new poly'),
 ];
 
 const Bumpers = [
